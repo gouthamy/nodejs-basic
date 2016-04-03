3876c9894b57ad82901bb3a38418ffe0744e220f
gouthamy <goutham.y@outlook.com>
2016-04-03 20:25:55 +0530

    Node js basic initial commit

---
+++ test/test.js
@@ -0,0 +1,8 @@
+
+var expect = require('chai').expect;
+
+describe('Node js basic test',function(){
+  it('should pass when everything is okay', function(){
+    expect(true).to.be.true;
+  });
+});
